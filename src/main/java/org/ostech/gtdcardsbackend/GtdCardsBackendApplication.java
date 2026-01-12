ceec0b955d6a36e6397ad901a412b3336bbeb33a
amilasilva7 <osapsilva@gmail.com>
2026-01-12 20:51:58 +0530

    Initial commit: Enterprise GTD Cards Backend

---
+++ src/main/java/org/ostech/gtdcardsbackend/GtdCardsBackendApplication.java
@@ -0,0 +1,13 @@
+package org.ostech.gtdcardsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GtdCardsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GtdCardsBackendApplication.class, args);
+    }
+
+}
